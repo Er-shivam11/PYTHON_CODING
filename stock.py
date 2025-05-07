a3de99d0b4e144947e173d7d12e620a3cd768d45
Er-shivam11 <shivamnirmal.097@gmail.com>
2025-05-07 20:26:18 +0530

    b&s stock

--- stock.py
+++ stock.py
@@ -1,4 +1,15 @@
 # Input: prices = [7,1,5,3,6,4]
 # Output: 5
+# def stocksell(price):
+prices = [7, 1, 5, 3, 6, 4]
+min_price = float('inf')
+max_profit=0
+for price in prices:
+    if price < min_price:
+        min_price = price
+    elif price - min_price > max_profit:
+        max_profit=price-min_price
+print(max_profit)
 
-prices = [7,1,5,3,6,4]
+
+# print(min_price)
