6cc3e7372d6f5ebba4d9098340ae97d8049dedc0
Er-shivam11 <shivamnirmal.097@gmail.com>
2025-05-09 11:58:37 +0530

    int dic //

--- lonsubstr.py
+++
@@ -1,5 +0,0 @@
-a='abcbbvaa'
-b=set(a)
-print(len(b))
-
-# print(set(len(a)))
---
+++ proarr.py
@@ -0,0 +1,14 @@
+num=[1,2,3,4]
+prod=1
+prod_arr=[]
+arr=[]
+for i in num:
+    prod=prod*i
+for j in num:
+    # print(j)
+    product_arr=prod//j
+    prod_arr.append(product_arr)
+print(prod_arr)
+
+
+
